4301aeb94ff7f5dc59325685171289e297e05cf5
belajarfrom0-creator <belajar.from0@gmail.com>
2026-06-11 20:19:13 +0700

    Delete frontend directory

--- frontend/globals.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.css'
-declare module '@/globals.css' {
-  const content: string;
-  export default content;
-}
-declare module '../globals.css' {
-  const content: string;
-  export default content;
-}
